e9615e74a6e606f34e52bbc721aeb725365cab75
Androv14 <shykoryak1111@gmail.com>
2024-03-25 06:38:44 +0200

    Worked with admin-console

--- pydjango/crm/models.py
+++ pydjango/crm/models.py
@@ -3,5 +3,10 @@ from django.db import models
 # Create your models here.
 class Order(models.Model):
     oreder_dt = models.DateTimeField(auto_now=True)
-    order_name = models.CharField(max_length=200)
-    order_phone = models.CharField(max_length=200)
+    order_name = models.CharField(max_length=200,verbose_name='Name')
+    order_phone = models.CharField(max_length=200,verbose_name='Phone')
+    def __str__(self) -> str:
+        return self.order_name
+    class Meta:
+        verbose_name='Order'
+        verbose_name_plural = 'Orders'
